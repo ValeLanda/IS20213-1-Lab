924eaecc04f3532dd1508b9d085332801baf05ce
Orlando <shamkt04@ciencias.unam.mx>
2022-09-05 15:36:39 -0500

    commit de prueba

---
+++ untitled1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 15:29:42 2022
+
+@author: O.Ledesma
+"""
+
+def mayor(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+    
+def longitud(l):
+    longitud = 0
+    for n in l:
+        longitud = longitud + 1
+    return longitud
+
